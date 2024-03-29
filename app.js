3d980c91e49e4bbc9cc6a30393f30f7835eab38a
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-29 07:35:12 -0400

    intro-comps: remove unused code

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const app = Vue.createApp({
     data() {
         return {
-            friends: [
-                { id: 'manual', name: 'Manuel Lorenz', phone: '324234234', email: 'asdas@local.com' },
-                { id: 'julie', name: 'Julie Lorenz', phone: '3242353453', email: 'sadfa@local.com' },
-            ]
         }
     },
     methods: {
