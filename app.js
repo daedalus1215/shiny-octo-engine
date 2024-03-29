0be837ec530437a83162bc53e3ce714e7f6f939d
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-29 07:29:17 -0400

    intro-comps: create initial friend-contact component

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const app = Vue.createApp({
     data() {
         return {
-            detailsAreVisible: false,
             friends: [
                 { id: 'manual', name: 'Manuel Lorenz', phone: '324234234', email: 'asdas@local.com' },
                 { id: 'julie', name: 'Julie Lorenz', phone: '3242353453', email: 'sadfa@local.com' },
             ]
+        }
+    },
+    methods: {
+
+    }
+})
+
+app.component('friend-contact', {
+    template: `
+    <li :key="friend.id">
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{detailsAreVisible ? 'Hide':'Show'}} Details</button>
+        <ul v-if="detailsAreVisible">
+          <li><strong>Phone:</strong>{{friend.phone}}</li>
+          <li><strong>Email:</strong> manuel@localhost.com</li>
+        </ul>
+      </li>
+    `,
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend:  { id: 'manual', name: 'Manuel Lorenz', phone: '324234234', email: 'asdas@local.com' }
         }
     },
     methods: {
