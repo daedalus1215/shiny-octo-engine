a83accf88956580bf7aca6c45cca6b85c319a6dc
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-30 16:43:16 -0400

    begin creating a new project

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,24 @@
-<script setup>
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-
-    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
-    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
-    <ActiveGoals v-if="selectedComponent === 'active-goals'"></ActiveGoals>
-    <ManageGoals v-if="selectedComponent === 'manage-goals'"></ManageGoals>
-  </div>
+  <ul>
+    <learning-resource v-for="resource in storedResources" :key="resource.id"></learning-resource>
+  </ul>
 </template>
 
-
 <script>
-import ActiveGoals from './components/ActiveGoals.vue'
-import ManageGoals from './components/ManageGoals.vue'
+import ResourceItem from './components/learning-resources/ResourceItem.vue';
 
 export default {
   components: {
-    ActiveGoals,
-    ManageGoals
-  },
+    ResourceItem
+  }
   data() {
     return {
-      selectedComponent: 'active-goals'
-    }
-  },
-  methods: {
-    setSelectedComponent(cmp) {
-      this.selectedComponent = cmp;
+      storedResources: [
+        { id: 'official-guide', title: 'Official Guide', description: 'The official vueJS documentation', link: 'https://vuejs.org' },
+        { id: 'google', title: 'Google', description: 'Search Engine', link: 'https://google.com' },
+
+      ]
     }
   }
 }
 </script>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/learning-resources/ResourceItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <li>
+        <div>
+            <header>
+                <h3></h3>
+                <button>Delete</button>
+            </header>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+
+}
+</script>
