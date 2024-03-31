2b186290ae30fc10a8fbfa6c63686f59eb092bf0
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-30 21:45:08 -0400

    Begin to addResource component

--- src/components/learning-resources/TheResource.vue
+++ src/components/learning-resources/TheResource.vue
@@ -1,7 +1,10 @@
 <template>
     <base-card>
-        <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
-        <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
+        <base-button @click="setSelectedTab('stored-resources')"
+            :mode="storedResButtonMode">
+            Stored Resources
+        </base-button>
+        <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode">Add Resource</base-button>
     </base-card>
     <component :is="selectedTab"></component>
 </template>
@@ -25,12 +28,31 @@ export default {
     },
     provide() {
         return {
-            resources: this.storedResources
+            resources: this.storedResources,
+            addResource: this.addResource
+        }
+    },
+    computed: {
+        storedResButtonMode() {
+            return this.selectedTab === 'stored-resources' ? null : 'flat';
+        },
+        addResButtonMode() {
+            return this.selectedTab === 'add-resource' ? null : 'flat';
         }
     },
     methods: {
         setSelectedTab(tab) {
             this.selectedTab = tab;
+        },
+        addResource(title, description, url) {
+            const newResource = {
+                id: new Date().toString(),
+                title: title,
+                description:description,
+                link: url
+            };
+            this.storedResources.unshift(newResource);
+            this.selectedTab = 'stored-resources';
         }
     }
 }
